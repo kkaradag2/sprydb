6e5be3cc18b5b7f5640899c99308b3b4afe8b552
kemal.karadag@bilgi.edu.tr <kemal.karadag@bilgi.edu.tr>
2017-05-12 14:31:03 +0300

    issue #3 Task-2 Task-3 Task-4

--- Spry/SpryDB/Options/ConfigurationOptions.cs
+++ Spry/SpryDB/Options/ConfigurationOptions.cs
@@ -5,6 +5,16 @@ namespace SpryDB.Options
     [Verb("Configuration", HelpText = "Clone a repository into a new directory.")]
     class ConfigurationOptions
     {
+
+        [Option('s', "set-config", HelpText = "Set new configration. if you already have, this comment overwrite it.")]
+        public bool setConfig { get; set; }
+
+        [Option('l', "list-config", HelpText = "Display your current configration.")]
+        public bool listConfig { get; set; }
+
+        [Option('t', "test-config", HelpText = "Test your current configration.")]
+        public bool testConfig { get; set; }
+
         public int RunConfigurationAndReturnExitCode(ConfigurationOptions opts)
         {
             return 0;
