c1b29c91ece609e3faf1f9b18aa0ba6d04334c44
kemal.karadag@bilgi.edu.tr <kemal.karadag@bilgi.edu.tr>
2017-05-16 16:48:06 +0300

    ConsoleTables installed on nuged. Configuration save is developed

--- Spry/SpryDB/Options/ConfigurationOptions.cs
+++ Spry/SpryDB/Options/ConfigurationOptions.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using CommandLine;
-using CommandLine.Text;
 using System.Reflection;
 using System.Drawing;
 using Console = Colorful.Console;
 using System.Text.RegularExpressions;
-using SpryDB.Models;
+using static SpryDB.Models.settings;
+using SpryDB.Settings;
+using System.Linq;
+using ConsoleTables;
 
 namespace SpryDB.Options
 {
-    [Verb("Configuration", HelpText = "Clone a repository into a new directory.")]
+    [Verb("Config", HelpText = "Used to organize, list and test the configuration data.")]
     class ConfigurationOptions
     {
 
-        [Option('s', "set-config", HelpText = "Set new configration. if you already have, this comment overwrite it.")]
-        public bool setConfig { get; set; }
+        [Option('s', "server", HelpText = "It only accepts MSSQL, ORACLE and MYSQL words. Default value is MSSQL")]
+        public string serverType { get; set; }
+
+        [Option('c', "connection", HelpText = "Used to define a ConnectionString for the database you will work with")]
+        public string connectionString { get; set; }
+
+        [Option('w', "directory", HelpText = "This key is used to define the directory or shared folder you want to work on")]
+        public string workingDirectory { get; set; }
 
         [Option('l', "list-config", HelpText = "Display your current configration.")]
         public bool listConfig { get; set; }
@@ -24,123 +32,109 @@ namespace SpryDB.Options
 
         public int RunConfigurationAndReturnExitCode()
         {
+
+            var config = new AllSettings();
+
+            if(!string.IsNullOrEmpty(serverType))
+            { 
+                if (Enum.IsDefined(typeof(ServerTypes), serverType))
+                {
+                    config.ServerType.Write(serverType);
+                }else
+                {
+                    Parser.Default.ParseArguments<ConfigurationOptions>(new string[] { "verb", "--help" });
+                }
+            }
+
             
-            if (this.setConfig)
-                SaveNewConfigration();
 
-            if (this.listConfig)
-                ListConfigration();
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                config.ConnectionString.Write(connectionString);
+            }
 
-            if (this.testConfig)
-                TestConfigration();
+            if(!string.IsNullOrEmpty(workingDirectory))
+            {
+                Regex folder = new Regex(@"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\)*\w([\w.])+$");  // Folder regex  c:\sql\working
+                Regex share = new Regex(@"^(\\)(\\[A-Za-z0-9-_]+){2,}(\\?)$");
 
-            if(isNoArgument()) // Run help if no argument is input for configuration command.
+                Match machFolder = folder.Match(workingDirectory);
+                Match machShare = share.Match(workingDirectory);
+
+                if (machFolder.Success || machShare.Success)
+                {
+                    config.WorkingDirectory.Write(workingDirectory);
+                }else
+                {
+                    Console.WriteLine("Pleace enter path or shared folder.", Color.Red);
+                    Parser.Default.ParseArguments<ConfigurationOptions>(new string[] { "verb", "--help" });
+                }                    
+
+            }
+
+            if(listConfig)
+            {
+                try
+                {
+                    var table = new ConsoleTable("PROPERTIES", "VALUES");
+                    table.AddRow("Database Server", config.ServerType)
+                         .AddRow("Connection String", config.ConnectionString)
+                         .AddRow("Working Directory", config.WorkingDirectory);
+
+                    Console.WriteLine("\nCurrent Configuration:\n", Color.DarkGreen);
+                    table.Write();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("not defined.", Color.Red);
+                    Console.WriteLine("");
+                    Parser.Default.ParseArguments<ConfigurationOptions>(new string[] { "verb", "--help" });
+                }
+                
+            }
+
+            if(testConfig)
+            {
+                Console.WriteLine("test configuration");
+            }
+
+            if (isAllPropertiesEmpty())
                 Parser.Default.ParseArguments<ConfigurationOptions>(new string[] { "verb", "--help" });
-
 
             return 0;
         }
 
-        private void TestConfigration()
+
+      
+
+        public bool isAllPropertiesEmpty()
         {
-            Console.WriteLine("Test configration");            
-        }
-
-        private void ListConfigration()
-        {
-            Console.WriteLine("List configration");
-        }
-
-        private void SaveNewConfigration()
-        {
-            var setting = new settings();
-
-            Console.WriteLine("");
-            Console.WriteLine("Pleace select your database server:",Color.White);
-            Console.WriteLine("");
-            Console.WriteLine("[1] MS SQL server", Color.White);
-            Console.WriteLine("[2] ORACLE server", Color.White);
-            Console.WriteLine("[3] MYSQL server", Color.White);
-            Console.WriteLine("[0] Exit", Color.White);
-            Console.WriteLine("");
-            Console.WriteLine("------------------------------------",Color.White);
-            Console.Write("Please enter a value between 0 and 3: ", Color.DarkGreen);
-
-            Regex regexMenu = new Regex(@"([0-4])");            
-            string menu = Console.ReadLine();
+            PropertyInfo[] properties = this.GetType().GetProperties();
+            int nullCount = 0;
             
-            while (!regexMenu.IsMatch(menu))
+            foreach (var pi in properties)
             {
-                Console.WriteLine("Bad input", Color.Red);
-                Console.Write("Please enter a value between 0 and 3: ", Color.DarkGreen);
-                menu = Console.ReadLine();
+                if (pi.PropertyType == typeof(string))         // To find any string propery is null or empty
+                {
+                    string value = (string)pi.GetValue(this);
+                    if (string.IsNullOrEmpty(value)) { nullCount++; }
+                }
+
+                if (pi.PropertyType == typeof(bool))        // To find andy bool propery is null or empty
+                {
+                    bool value = (bool)pi.GetValue(this);
+                    if (!value) { nullCount++; }
+                }
             }
 
-
-
-
-
-            Regex folder = new Regex(@"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\)*\w([\w.])+$");  // Folder regex  c:\sql\working
-            Regex share = new Regex(@"^(\\)(\\[A-Za-z0-9-_]+){2,}(\\?)$");
-          
-            
-
-
-
-
-            string connectionstring = string.Empty;
-            string workingdirectory = string.Empty;
-
-            //try
-            //{
-                
-            //    opcao = int.Parse(Console.ReadLine());
-            //    switch (opcao)
-            //    {
-            //        case 1:     // MS SQL Server
-            //            setting.Server = settings.ServerTypes.MSSQL;
-            //            break;
-            //        case 2:     // ORACLE Server
-            //            setting.Server = settings.ServerTypes.ORACLE;
-            //            break;
-            //        case 3:     // MYSQL Server
-            //            setting.Server = settings.ServerTypes.MYSQL;
-            //            break;
-            //        case 0:
-            //            Environment.Exit(0);
-            //            break;
-            //        default:
-            //            throw new Exception("Pleace select 0-3 arange");
-            //            break;
-            //    }
-
-            //    Regex folder = new Regex(@"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\)*\w([\w.])+$");  // Folder regex  c:\sql\working
-            //    Regex share = new Regex(@"^(\\)(\\[A-Za-z0-9-_]+){2,}(\\?)$");
-
-             
-            //}
-            //catch (Exception exp)
-            //{
-            //    Console.WriteLine(exp.Message, Color.Red);
-            //    Console.WriteLine("-------------------------------------", Color.White);
-            //    SaveNewConfigration();
-            //}
-            
-        }
-        
-
-        private bool isNoArgument()
-        {           
-
-            foreach (PropertyInfo pi in this.GetType().GetProperties())
-            {
-                bool value = (bool)pi.GetValue(this);
-                if (value)
-                    return false;
-            }
+            if (nullCount == properties.Count())
                 return true;
+
+            return false;
         }
-        
+
+
+       
       
 
     }
---
+++ Spry/SpryDB/Settings/AllSettings.cs
@@ -0,0 +1,19 @@
+﻿using Config.Net;
+using System;
+
+namespace SpryDB.Settings
+{
+    public class AllSettings : SettingsContainer
+    {
+        
+        public readonly Option<string> ServerType;
+        public readonly Option<string> WorkingDirectory;
+        public readonly Option<string> ConnectionString;
+
+    
+        protected override void OnConfigure(IConfigConfiguration configuration)
+        {            
+            configuration.UseJsonFile(string.Format(@"{0}\config.json", AppDomain.CurrentDomain.BaseDirectory));
+        }
+    }
+}
