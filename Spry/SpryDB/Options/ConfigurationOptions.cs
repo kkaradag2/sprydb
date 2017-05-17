8c027f967628d4c4cad5503f5f807f1d7f0d18cc
kemal.karadag@bilgi.edu.tr <kemal.karadag@bilgi.edu.tr>
2017-05-17 12:09:59 +0300

    issues#3 is complated

--- Spry/SpryDB/Options/ConfigurationOptions.cs
+++ Spry/SpryDB/Options/ConfigurationOptions.cs
@@ -8,6 +8,8 @@ using static SpryDB.Models.settings;
 using SpryDB.Settings;
 using System.Linq;
 using ConsoleTables;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace SpryDB.Options
 {
@@ -35,7 +37,10 @@ namespace SpryDB.Options
 
             var config = new AllSettings();
 
-            if(!string.IsNullOrEmpty(serverType))
+            // Section of Defination Server Type
+            // SAMPLE : Config -s ORACLE
+            // SAMPLE : Config -s MYSQL
+            if (!string.IsNullOrEmpty(serverType))
             { 
                 if (Enum.IsDefined(typeof(ServerTypes), serverType))
                 {
@@ -46,13 +51,16 @@ namespace SpryDB.Options
                 }
             }
 
-            
-
+            // Section of Defination Database connection: 
+            // SAMPLE : Config -c Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;
             if (!string.IsNullOrEmpty(connectionString))
             {
                 config.ConnectionString.Write(connectionString);
             }
 
+            // Section of defination working directory
+            // SAMPLE : Config -w c:\test\sql
+            // SAMPLE : Config -w \\FileServer\SQL\Deployments\
             if(!string.IsNullOrEmpty(workingDirectory))
             {
                 Regex folder = new Regex(@"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\)*\w([\w.])+$");  // Folder regex  c:\sql\working
@@ -72,30 +80,54 @@ namespace SpryDB.Options
 
             }
 
+            // Display curreten configurations
+            // SAMPLE :  Config -l
             if(listConfig)
             {
                 try
                 {
-                    var table = new ConsoleTable("PROPERTIES", "VALUES");
-                    table.AddRow("Database Server", config.ServerType)
-                         .AddRow("Connection String", config.ConnectionString)
-                         .AddRow("Working Directory", config.WorkingDirectory);
-
                     Console.WriteLine("\nCurrent Configuration:\n", Color.DarkGreen);
-                    table.Write();
+
+                    var table = new ConsoleTable("Properties", "Values");
+                    table.AddRow("Database Server", config.ServerType ?? "Not Defined")
+                         .AddRow("Connection String", config.ConnectionString ?? "Not Defined")
+                         .AddRow("Working Directory", config.WorkingDirectory ?? "Not Defined");
+                    table.Write();                    
                 }
                 catch (Exception)
-                {
-                    Console.WriteLine("not defined.", Color.Red);
-                    Console.WriteLine("");
-                    Parser.Default.ParseArguments<ConfigurationOptions>(new string[] { "verb", "--help" });
+                {                    
+                    Console.WriteLine("Configuration is not defined.", Color.Red);
+                    Console.WriteLine("You can learn how to define the configuration with the help Config.");                    
                 }
                 
             }
 
-            if(testConfig)
+            // Test curreten configurations. Check database connection is working, Check working directory is accessible.
+            // SAMPLE :  Config -t
+            if (testConfig)
             {
-                Console.WriteLine("test configuration");
+                var table = new ConsoleTable("Properties", "Values", "Status");
+
+                if(!string.IsNullOrEmpty(config.ConnectionString))
+                {
+                    if(string.IsNullOrEmpty(config.ServerType))
+                    {
+                        config.ServerType.Write("MSSQL"); // Defaul value is setting.
+                    }
+                    table.AddRow("Connection String", config.ConnectionString, CheckConnectionString(config));
+                    if (Directory.Exists(config.WorkingDirectory))
+                    {
+                        table.AddRow("Working Directory", config.WorkingDirectory ?? "null", "Success");
+                    }else
+                    {
+                        table.AddRow("Working Directory", config.WorkingDirectory ?? "null", "Fail");
+                    }
+                }
+                else
+                {
+                    table.AddRow("Connection String", "not defined", "Fail");
+                }
+                table.Write();
             }
 
             if (isAllPropertiesEmpty())
@@ -104,8 +136,15 @@ namespace SpryDB.Options
             return 0;
         }
 
-
-      
+        private string CheckConnectionString(AllSettings config)
+        {
+            if(config.ServerType == "MSSQL")
+            {
+                try {using (new SqlConnection(config.ConnectionString)) { } return "Success"; }
+                catch (SqlException ex) { return string.Format("{0} - {1}", "Fail", ex.Message); }                
+            }
+            return "Fail";
+        }
 
         public bool isAllPropertiesEmpty()
         {
@@ -133,7 +172,28 @@ namespace SpryDB.Options
             return false;
         }
 
+        // This function is developed for understant currentconfiguration is sutible to migration.
+        public static bool TestConfiguration()
+        {
+            var config = new AllSettings();
 
+            if (string.IsNullOrEmpty(config.ConnectionString))
+            {
+                return false;
+            }else
+            {
+                if (config.ServerType == "MSSQL")
+                {
+                    try { using (new SqlConnection(config.ConnectionString)) { } }
+                    catch (SqlException) { return false; }
+                }
+            }
+
+            if (!Directory.Exists(config.WorkingDirectory))
+                return false;
+
+            return true;
+        }
        
       
 
